6fe027535f17fa128e291760e142188334b04300
mattyv316 <mattyv316@gmail.com>
2020-04-27 10:11:40 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'learning.puppet.vm'{
   include role::master_server
    file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}",
   }
 }
 node /^web/ {
