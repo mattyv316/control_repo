d21f56f2fccbbea36f300b9d6d5341e5d0b4a331
mattyv316 <mattyv316@gmail.com>
2020-04-27 13:34:34 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,10 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node{'web.puppetlabs.vm':}
   dockeragent::node{'db.puppetlabs.vm':}
+  host {'web.puppet.vm':
+    ensure => present,
+  }
+  host {'db.puppet.vm':
+    ensure => present,
+  }
 }
