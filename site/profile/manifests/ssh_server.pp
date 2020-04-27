a9506a7d507a94ee9dbff52d4e47e2e40714d2e7
mattyv316 <mattyv316@gmail.com>
2020-04-27 14:14:31 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,3 +12,4 @@ class profile::ssh_server {
     type   => 'ssh-rsa',
     key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC8mOS2HsjkCrtOctWWft3a89/ziYICWvVdr1++wwvvdPF7DhWWdUZY1MRAbwpXPjB4w+GQ/ws5l0Ln//Nz6hc5Mgv6sv2C5zdZVjOc/w8Cqp3P/QCzVajMqONUIiYaf+XfAWI88nBKFBwEn4ScshVPZ9HL2opEqqBU8VLMUVZYLxhs/1IR0Z8QqyIjSt5JL+yJp2zTLwzJqQVuGBgHvAi4j71H4aRFScBzxHUIkmlk5JEpSAuIc2z9eVbievnYkndkFWQQ/8O9DNuC7IZ4IfWOJDH1Fmu4sacw9YDaJoLsloic15DLy5dZjethnp2cazDfBS7pZaYn5SHRC9ihbR3p',
   } 
+ }
